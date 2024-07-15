b34146e65222ab9d162e0ab439834887f48dc0e1
Boris Randebrock <brandebr@student.42barcelona.com>
2024-07-15 15:14:22 +0200

    merged and normed

--- src/assets_manager.c
+++ src/assets_manager.c
@@ -6,38 +6,42 @@
 #include "cube3d.h"
 #include "libft.h"
 
-#define ELEMENT_PATH 3
+#define OFFSET 3
 
 static char	*_load_textures_path(char *buffer)
 {
 	char	*path;
+
 	path = ft_strdup(buffer);
 	*(ft_strchr(path, '\n')) = '\0';
 	return (path);
-} 
+}
 
-static void	_load_textures(int fd, char *buffer, int i, t_datamodel *datamodel)
+static void	_load_textures( t_datamodel *datamodel, char *buffer, int fd)
 {
-		 while (read(fd, &buffer[i], 1))
+	int		i;
+
+	i = 0;
+	while (read(fd, &buffer[i], 1))
 	{
 		if (buffer[0] == '\n')
-			break;
+			break ;
 		if (buffer[i] == '\n')
 		{
 			if (buffer[0] == 'N' && buffer[1] == 'O')
-				datamodel->no_tex_path = _load_textures_path(&buffer[ELEMENT_PATH]);
+				datamodel->no_tex_path = _load_textures_path(&buffer[OFFSET]);
 			if (buffer[0] == 'S' && buffer[1] == 'O')
-				datamodel->so_tex_path = _load_textures_path(&buffer[ELEMENT_PATH]);
+				datamodel->so_tex_path = _load_textures_path(&buffer[OFFSET]);
 			if (buffer[0] == 'W' && buffer[1] == 'E')
-				datamodel->we_tex_path = _load_textures_path(&buffer[ELEMENT_PATH]);
+				datamodel->we_tex_path = _load_textures_path(&buffer[OFFSET]);
 			if (buffer[0] == 'E' && buffer[1] == 'A')
-				datamodel->ea_tex_path = _load_textures_path(&buffer[ELEMENT_PATH]);
+				datamodel->ea_tex_path = _load_textures_path(&buffer[OFFSET]);
 			ft_memset(buffer, '\0', BUFSIZ);
 			i = 0;
 			continue ;
 		}
 		i++;
-	} 
+	}
 	ft_memset(buffer, '\0', BUFSIZ);
 }
 
@@ -47,8 +51,9 @@ static void	_load_map_data(t_datamodel *datamodel, t_list *row_list)
 	int		i;
 
 	i = 0;
-	datamodel->tilemap->map = __calloc(datamodel->tilemap->size.y, sizeof(char *));
-	while(i < datamodel->tilemap->size.y)
+	datamodel->tilemap->map = __calloc(datamodel->tilemap->size.y,
+			sizeof(char *));
+	while (i < datamodel->tilemap->size.y)
 	{
 		head = row_list;
 		datamodel->tilemap->map[i++] = head->content;
@@ -71,14 +76,14 @@ static void	_load_tilemap(t_datamodel *datamodel, char *buffer, int fd)
 		if (buffer[i] == '\n')
 		{
 			row = ft_strdup(buffer);
-			row[ft_strlen(row) - 1] = '\0'; 
+			row[ft_strlen(row) - 1] = '\0';
 			ft_lstadd_back(&row_list, ft_lstnew(row));
 			ft_memset(buffer, '\0', BUFSIZ);
 			if (i > datamodel->tilemap->size.x)
 				datamodel->tilemap->size.x = i;
 			datamodel->tilemap->size.y++;
 			i = 0;
-			continue;
+			continue ;
 		}
 		i++;
 	}
@@ -86,7 +91,7 @@ static void	_load_tilemap(t_datamodel *datamodel, char *buffer, int fd)
 }
 
 int	_rgbtohex(char *buffer)
-{	
+{
 	int		hex_color;
 	char	*rgb;
 	char	**split_values;
@@ -118,7 +123,7 @@ static void	_load_surfaces_color(t_datamodel *datamodel, char *buffer, int fd)
 	while (read(fd, &buffer[i], 1))
 	{
 		if (buffer[0] == '\n')
-			break;
+			break ;
 		if (buffer[i] == '\n')
 		{
 			if (buffer[0] == 'F' || buffer[0] == 'C')
@@ -140,19 +145,14 @@ static void	_load_surfaces_color(t_datamodel *datamodel, char *buffer, int fd)
 
 t_datamodel	*load_cub(char *filename)
 {
-	int		fd;
-	int		i;
-	char	buffer[BUFSIZ];
-	t_datamodel *datamodel;
+	t_datamodel	*datamodel;
+	int			fd;
+	char		buffer[BUFSIZ];
 
 	datamodel = __calloc(1, sizeof(t_datamodel));
 	ft_memset(buffer, '\0', BUFSIZ);
-
-	//LOAD ELEMENT PATHS
 	fd = open(filename, O_RDONLY, 0777);
-
-	i = 0;
-	_load_textures(fd, buffer, i, datamodel);
+	_load_textures(datamodel, buffer, fd);
 	_load_surfaces_color(datamodel, buffer, fd);
 	_load_tilemap(datamodel, buffer, fd);
 	close(fd);
