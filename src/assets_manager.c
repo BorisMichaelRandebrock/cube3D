bfcee9db0dc90f0591a0123efbdf2685b1cc8ad1
Lineath <fran.montserrat.developer@gmail.com>
2024-07-15 14:40:06 +0200

    norm fix cub loader

--- src/assets_manager.c
+++ src/assets_manager.c
@@ -8,6 +8,106 @@
 
 #define ELEMENT_PATH 3
 
+static void	_load_map_data(t_datamodel *datamodel, t_list *row_list)
+{
+	t_list	*head;
+	int		i;
+
+	i = 0;
+	datamodel->tilemap->map = __calloc(datamodel->tilemap->size.y, sizeof(char *));
+	while(i < datamodel->tilemap->size.y)
+	{
+		head = row_list;
+		datamodel->tilemap->map[i++] = head->content;
+		row_list = row_list->next;
+		free(head);
+	}
+}
+
+static void	_load_tilemap(t_datamodel *datamodel, char *buffer, int fd)
+{
+	char	*row;
+	t_list	*row_list;
+	int		i;
+
+	datamodel->tilemap = __calloc(1, sizeof(t_tilemap));
+	row_list = NULL;
+	i = 0;
+	while (read(fd, &buffer[i], 1))
+	{
+		if (buffer[i] == '\n')
+		{
+			row = ft_strdup(buffer);
+			row[ft_strlen(row) - 1] = '\0'; 
+			ft_lstadd_back(&row_list, ft_lstnew(row));
+			ft_memset(buffer, '\0', BUFSIZ);
+			if (i > datamodel->tilemap->size.x)
+				datamodel->tilemap->size.x = i;
+			datamodel->tilemap->size.y++;
+			i = 0;
+			continue;
+		}
+		i++;
+	}
+	_load_map_data(datamodel, row_list);
+}
+
+// 22222222
+
+int	_rgbtohex(char *buffer)
+{	
+	int		hex_color;
+	char	*rgb;
+	char	**split_values;
+	int		temp;
+
+	rgb = ft_strdup(&buffer[2]);
+	split_values = ft_split(rgb, ',');
+	hex_color = 0;
+	temp = ft_atoi(split_values[0]);
+	temp <<= 24;
+	hex_color |= temp;
+	temp = ft_atoi(split_values[1]);
+	temp <<= 16;
+	hex_color |= temp;
+	temp = ft_atoi(split_values[2]);
+	temp <<= 8;
+	hex_color |= temp;
+	free(rgb);
+	free(split_values);
+	return (hex_color);
+}
+
+static void	_load_surfaces_color(t_datamodel *datamodel, char *buffer, int fd)
+{
+	int	color;
+	int	i;
+
+	i = 0;
+	while (read(fd, &buffer[i], 1))
+	{
+		if (buffer[0] == '\n')
+			break;
+		if (buffer[i] == '\n')
+		{
+			if (buffer[0] == 'F' || buffer[0] == 'C')
+			{
+				color = _rgbtohex(buffer);
+				if (buffer[0] == 'F')
+					datamodel->floor_color = color;
+				else if (buffer[0] == 'C')
+					datamodel->ceiling_color = color;
+			}
+			ft_memset(buffer, '\0', BUFSIZ);
+			i = 0;
+			continue ;
+		}
+		i++;
+	}
+	ft_memset(buffer, '\0', BUFSIZ);
+}
+
+
 t_datamodel	*load_cub(char *filename)
 {
 	int		fd;
@@ -60,81 +160,9 @@ t_datamodel	*load_cub(char *filename)
 		i++;
 	}
 
-	//LOAD AND CONVERT RGBA TO HEX
-	i = 0;
-	while (read(fd, &buffer[i], 1))
-	{
-		if (buffer[0] == '\n')
-		{
-			ft_memset(buffer, '\0', BUFSIZ);
-			break;
-		}
-		if (buffer[i] == '\n')
-		{
-			if (buffer[0] == 'F' || buffer[0] == 'C')
-			{
-				int	color;
-
-				char *rgb = ft_strdup(&buffer[2]);
-				char **split_values = ft_split(rgb, ',');
-				color = 0;
-				int	temp = ft_atoi(split_values[0]);
-				temp <<= 24;
-				color |= temp;
-				temp = ft_atoi(split_values[1]);
-				temp <<= 16;
-				color |= temp;
-				temp = ft_atoi(split_values[2]);
-				temp <<= 8;
-				color |= temp;
-				free(rgb);
-				free(split_values);
-				if (buffer[0] == 'F')
-					datamodel->floor_color = color;
-				else if (buffer[0] == 'C')
-					datamodel->ceiling_color = color;
-			}
-			ft_memset(buffer, '\0', BUFSIZ);
-			i = 0;
-			continue ;
-		}
-		i++;
-	}
-
-	//LOAD MAP
-	char	*row;
-	t_list	*head;
-	t_list	*row_list;
-
-	datamodel->tilemap = __calloc(1, sizeof(t_tilemap));
-
-	i = 0;
-	while (read(fd, &buffer[i], 1))
-	{
-		if (buffer[i] == '\n')
-		{
-			row = ft_strdup(buffer);
-			row[ft_strlen(row) - 1] = '\0'; 
-			ft_lstadd_back(&row_list, ft_lstnew(row));
-			ft_memset(buffer, '\0', BUFSIZ);
-			if (i > datamodel->tilemap->size.x)
-				datamodel->tilemap->size.x = i;
-			datamodel->tilemap->size.y++;
-			i = 0;
-			continue;
-		}
-		i++;
-	}
+	_load_surfaces_color(datamodel, buffer, fd);
+	_load_tilemap(datamodel, buffer, fd);
 	close(fd);
-
-	datamodel->tilemap->map = __calloc(datamodel->tilemap->size.y, sizeof(char *));
-	i = 0;
-	while(i < datamodel->tilemap->size.y)
-	{
-		head = row_list;
-		datamodel->tilemap->map[i++] = head->content;
-		row_list = row_list->next;
-		free(head);
-	}
 	return (datamodel);
 }
+
